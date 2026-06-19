23e7ee400d802796c594f3c5a1be9cb06e064e28
JonatasGdeC <jonatas.07022003@gmail.com>
2026-06-19 20:00:46 -0300

    Criando novas páginas

---
+++ src/Mobile/PlanShare.App/ViewModels/Pages/User/Register/RegisterUserViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using PlanShare.App.Navigation;
+
+namespace PlanShare.App.ViewModels.Pages.User.Register;
+
+public partial class RegisterUserViewModel
+{
+    [RelayCommand]
+    private async Task LoginWithEmailAndPasswords() => await Shell.Current.GoToAsync(state: $"../{RoutePages.LOGIN_PAGE}");
+}
