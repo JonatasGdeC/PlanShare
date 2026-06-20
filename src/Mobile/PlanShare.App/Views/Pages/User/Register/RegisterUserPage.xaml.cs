13ca2d72681a89c84aea6f1aad4c3fbf06939dc9
JonatasGdeC <jonatas.07022003@gmail.com>
2026-06-20 20:09:11 -0300

    Criando label para títulos de entry

--- src/Mobile/PlanShare.App/Views/Pages/User/Register/RegisterUserPage.xaml.cs
+++ src/Mobile/PlanShare.App/Views/Pages/User/Register/RegisterUserPage.xaml.cs
@@ -1,3 +1,5 @@
+using PlanShare.App.ViewModels.Pages.User.Register;
+
 namespace PlanShare.App.Views.Pages.User.Register;
 
 public partial class RegisterUserPage : ContentPage
@@ -5,5 +7,6 @@ public partial class RegisterUserPage : ContentPage
     public RegisterUserPage()
     {
         InitializeComponent();
+        BindingContext = new RegisterUserViewModel();
     }
 }
